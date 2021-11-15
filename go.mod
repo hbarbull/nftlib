6c11bb497b7e59fcf7fc1248614488cabf48f522
Bob R <robertmrobersonjr@gmail.com>
2021-11-14 18:17:22 -0800

    added function for minting series

--- go.mod
+++ go.mod
@@ -2,4 +2,18 @@ module github.com/hbarbull/nftlib
 
 go 1.17
 
-require github.com/hashgraph/hedera-sdk-go/v2 v2.4.0
+require github.com/hashgraph/hedera-sdk-go/v2 v2.4.1
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210907225631-ff17edfbf26d // indirect
+	golang.org/x/sys v0.0.0-20210906170528-6f6e22806c34 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20210903162649-d08c68adba83 // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
--- lib.go
+++ lib.go
@@ -25,3 +25,27 @@ func CreateAndMintSingle(client *hedera.Client, nftStorageKey string, photoPath 
 	}
 	return nftId, nil
 }
+
+func CreateAndMintSeries(client *hedera.Client, nftStorageKey string, photoPath string,
+	name string, description string, creator string, category string, supply int64) ([]string, error) {
+	var nfts []string
+	cid, err := UploadNft(photoPath, name, description, creator, category, supply, nftStorageKey)
+	if err != nil {
+		return nfts, err
+	}
+	tokenSymbol := fmt.Sprintf("IPFS://%s", cid)
+	nftMetadata := fmt.Sprintf("https://cloudflare-ipfs.com/ipfs/%s", cid)
+	tokenId, err := CreateToken(client, name, tokenSymbol, supply)
+	if err != nil {
+		return nfts, err
+	}
+	var i int64
+	for i = 1; i <= supply; i++ {
+		nftId, err := MintToken(client, tokenId, nftMetadata)
+		nfts = append(nfts, nftId)
+		if err != nil {
+			return nfts, err
+		}
+	}
+	return nfts, nil
+}
